b2d3a3629f6e5b9911dd2939cbf2d418e9acb2d8
Karan Bopche <karanbopche@gmail.com>
2019-06-23 00:38:30 +0530

    initial

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#define DEAD	'X'
+#define ALIVE	'O'
+
+const int width = 40;
+const int height = 40;
+
+#define INDEX(r, c)	(r*width+c)
+
+int gameBoard1[height*width];	// current board....
+int gameBoard2[height*width];	// next generation board........
+
+int *gameBoardCurrent = gameBoard1;	// pointer to current gameboard
+int *gameBoardUpdate = gameBoard2;
+
+
+int main() {
+	
+	render();
+	return 0;
+}
+
+void render() {
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			if (gameBoardCurrent[INDEX(row, col)])
+				std::cout << ALIVE;
+			else
+				std::cout << DEAD;
+		}
+	}
+}
